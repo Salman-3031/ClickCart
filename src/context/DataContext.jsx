0badd2f44140d88dc1596a867bcdf90854aee312
Salman-3031 <salman5223031@gmail.com>
2025-09-27 12:04:12 +0500

    extra changes

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,36 +1,35 @@
 import axios from "axios";
 import { createContext, useContext, useState } from "react";
-import Category from "../components/Category";
 
 export const DataContext = createContext(null)
 
-export const DataProvider = ({children})=>{
-    const [data,setData] = useState()
+export const DataProvider = ({ children }) => {
+  const [data, setData] = useState()
 
-   const fetchingData = async () =>{
-     try {
-        const res = await axios.get(`https://fakestoreapi.in/api/products?limit=150`)
-        const productsData = res.data.products
-        // console.log(productsData)
-        setData(productsData)
+  const fetchingData = async () => {
+    try {
+      const res = await axios.get(`https://fakestoreapi.in/api/products?limit=150`)
+      const productsData = res.data.products
+      // console.log(productsData)
+      setData(productsData)
     } catch (error) {
-        console.log(error)
+      console.log(error)
     }
-   }
+  }
 
-//    fetchingData by Category 
+  //    fetchingData by Category 
   const getUniqueCategory = (data, property) => {
     let newVal = data?.map((currElem) => currElem[property])
-    newVal = ['All',...new Set(newVal)]
+    newVal = ['All', ...new Set(newVal)]
     return newVal
   }
 
   const categoryOnlyData = getUniqueCategory(data, 'category')
-  const brandOnlyData = getUniqueCategory(data,'brand')
+  const brandOnlyData = getUniqueCategory(data, 'brand')
 
-    return <DataContext.Provider value={{data,setData,fetchingData, categoryOnlyData,brandOnlyData}}>
-        {children}
-    </DataContext.Provider>
+  return <DataContext.Provider value={{ data, setData, fetchingData, categoryOnlyData, brandOnlyData }}>
+    {children}
+  </DataContext.Provider>
 }
 
 export const getData = () => useContext(DataContext)
